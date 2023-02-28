f607f2d661a16a0b615fed82904fdcb975edc7c4
Will <65666514+ictwill@users.noreply.github.com>
2023-02-28 17:35:20 -0500

    Fix IntrinsicHeight for number box

--- lib/src/screens/spots/cue_card.dart
+++ lib/src/screens/spots/cue_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:followspot_application_1/src/screens/spots/cue_edit_form.dart';
 
 import '../../models/cue.dart';
 import 'cue_edit_view.dart';
@@ -26,97 +27,101 @@ class CueCard extends StatelessWidget {
               showModalBottomSheet<void>(
                 context: context,
                 builder: (BuildContext context) {
-                  return CueEditView(spot: item.spot, cue: item);
+                  return CueEditForm(cue: item);
                 },
               );
             },
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: 96.0,
-                  height: 80.0,
-                  alignment: Alignment.center,
-                  color: item.maneuver?.getColor(),
-                  child: Text(deleteTrailing(item.number),
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                          color: Theme.of(context).colorScheme.onPrimary,
-                          fontWeight: FontWeight.bold)),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 8.0, horizontal: 16),
-                    child: Column(
-                      children: [
-                        Table(
-                          columnWidths: const <int, TableColumnWidth>{},
-                          children: [
-                            TableRow(
-                              children: [
-                                TableCell(
-                                    child: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Icon(
-                                      item.maneuver?.icon,
-                                      color: Color(
-                                          item.maneuver?.color ?? 0xFF77777),
-                                    ),
-                                    const SizedBox(width: 8.0),
-                                    Text(item.maneuver?.name ?? '-'),
-                                  ],
-                                )),
-                                Text(
-                                  item.target,
-                                  textAlign: TextAlign.center,
-                                ),
-                                Text(
-                                  item.size,
-                                  textAlign: TextAlign.end,
-                                ),
-                              ],
-                            ),
-                            TableRow(
-                              children: [
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 8.0),
-                                  child: Text(
-                                      '${validateIntensity(intensity: item.intensity)} %'),
-                                ),
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 8.0),
-                                  child: Text(
-                                    item.frames.join(' + '),
+            child: IntrinsicHeight(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 96.0,
+                    alignment: Alignment.center,
+                    color: item.maneuver?.getColor(),
+                    child: Text(deleteTrailing(item.number),
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium!
+                            .copyWith(
+                                color: Theme.of(context).colorScheme.onPrimary,
+                                fontWeight: FontWeight.bold)),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 16),
+                      child: Column(
+                        children: [
+                          Table(
+                            columnWidths: const <int, TableColumnWidth>{},
+                            children: [
+                              TableRow(
+                                children: [
+                                  TableCell(
+                                      child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Icon(
+                                        item.maneuver?.icon,
+                                        color: Color(
+                                            item.maneuver?.color ?? 0xFF77777),
+                                      ),
+                                      const SizedBox(width: 8.0),
+                                      Text(item.maneuver?.name ?? '-'),
+                                    ],
+                                  )),
+                                  Text(
+                                    item.target,
                                     textAlign: TextAlign.center,
                                   ),
-                                ),
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 8.0),
-                                  child: Text(
-                                    '${validateTime(time: item.time)} ct',
+                                  Text(
+                                    item.size,
                                     textAlign: TextAlign.end,
                                   ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        if (item.notes.isNotEmpty)
-                          Row(
-                            children: [
-                              Text(item.notes),
+                                ],
+                              ),
+                              TableRow(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 8.0),
+                                    child: Text(
+                                        '${validateIntensity(intensity: item.intensity)} %'),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 8.0),
+                                    child: Text(
+                                      item.frames.join(' + '),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 8.0),
+                                    child: Text(
+                                      '${validateTime(time: item.time)} ct',
+                                      textAlign: TextAlign.end,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ],
-                          )
-                      ],
+                          ),
+                          if (item.notes.isNotEmpty)
+                            Row(
+                              children: [
+                                Text(item.notes),
+                              ],
+                            )
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
@@ -129,3 +134,13 @@ String validateIntensity({int? intensity}) =>
     intensity != null && intensity >= 0 ? '$intensity' : '';
 
 String validateTime({int? time}) => time != null && time >= 0 ? '$time' : '';
+String deleteTrailing(double number) {
+  var string = number.toString();
+  while (string.endsWith('0') && string.contains('.0')) {
+    string = _dropLast(string);
+    if (string.endsWith('.')) return _dropLast(string);
+  }
+  return string;
+}
+
+String _dropLast(string) => string.substring(0, string.length - 1);
